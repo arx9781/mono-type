eceeb9e7ee6685bf1a6a3b547cfc4ca4ec6e9881
Aditya Roychoudhary <ar006985@gmail.com>
2025-10-13 03:55:14 +0530

    add: rate limiting

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
@@ -9,14 +10,15 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-connectDB();
-
 // middleware
 app.use(express.json());
+app.use(rateLimiter);
 
 // create route
 app.use("/api/notes", notesRoutes);
 
-app.listen(5001, () => {
-  console.log("Server started on PORT: ", PORT);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on PORT: ", PORT);
+  });
 });
---
+++ server/src/middleware/rateLimiter.js
@@ -0,0 +1,17 @@
+const rateLimiter = async (_, res, next) => {
+  try {
+    const { success } = await ratelimit.limit("limit_key");
+    if (!success) {
+      return res.status(429).json({
+        success: false,
+        message: "Too many requests, please try again later",
+      });
+    }
+    next();
+  } catch (error) {
+    console.error("Error in rateLimiter:", error);
+    next(error);
+  }
+};
+
+export default rateLimiter;
